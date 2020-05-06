5fac6f0f8283fa605fb6b32e3c1dfbef86ad11cb
Brandon Fuller <brandonfullerue4sc@gmail.com>
2020-05-06 15:26:31 +0100

    Initial commit

---
+++ Source/ShadowOfKnights/ShadowOfKnightsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShadowOfKnightsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWOFKNIGHTS_API AShadowOfKnightsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
