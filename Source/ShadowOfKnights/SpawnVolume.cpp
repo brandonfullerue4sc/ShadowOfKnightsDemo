7aee0041aae2c2aafc39714be8561e9de71c17f3
Brandon Fuller <brandonfullerue4sc@gmail.com>
2020-05-18 12:05:22 +0100

    Spawn volume (continued - part 1).

--- Source/ShadowOfKnights/SpawnVolume.cpp
+++ Source/ShadowOfKnights/SpawnVolume.cpp
@@ -4,6 +4,8 @@
 #include "SpawnVolume.h"
 #include "Components/BoxComponent.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "Engine/World.h"
+#include "Critter.h"
 
 // Sets default values
 ASpawnVolume::ASpawnVolume()
@@ -37,5 +39,16 @@ FVector ASpawnVolume::GetSpawnPoint()
 	FVector Point = UKismetMathLibrary::RandomPointInBoundingBox(Origin, Extent);
 
 	return Point;
+}
+
+void ASpawnVolume::SpawnOurPawn_Implementation(UClass* ToSpawn, const FVector& Location)
+{
+	UWorld* World = GetWorld();
+	FActorSpawnParameters SpawnParameters;
+
+	if (World)
+	{
+		ACritter* CritterSpawned = World->SpawnActor<ACritter>(ToSpawn, Location, FRotator(0.f), SpawnParameters);
+	}
 }
 
